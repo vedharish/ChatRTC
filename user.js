b202924d7de91e5cea51b8a9a08dbe4fe5f8a331
Harish Ved <ved.harish3@gmail.com>
2016-09-25 01:19:40 +0530

    UI Changes for video

--- user.js
+++ user.js
@@ -11,5 +11,24 @@ var User = {
     $("#sourceName").val(localStorage.sourcePeer);
     $("#welcomePeer").addClass("hidden");
     $("#enterName").removeClass("hidden");
+  },
+
+  switchToChat: function() {
+    $('#videoContent').addClass("hidden");
+    $('#chatContent').removeClass("hidden");
+    $('#showChatContent').addClass("hidden");
+    $('#showVideoContent').removeClass("hidden");
+    $('#chatTitleChatRTC').removeClass("hidden");
+    $('#chatTitleVideoRTC').addClass("hidden");
+  },
+
+  switchToVideo: function() {
+    console.log("Switching to VideoRTC");
+    $('#videoContent').removeClass("hidden");
+    $('#chatContent').addClass("hidden");
+    $('#showChatContent').removeClass("hidden");
+    $('#showVideoContent').addClass("hidden");
+    $('#chatTitleChatRTC').addClass("hidden");
+    $('#chatTitleVideoRTC').removeClass("hidden");
   }
 }
